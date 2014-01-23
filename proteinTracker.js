2ef903699f0e1fd68982f18153e20a9b1bafa42c
“Randy <rmarmer1@yahoo.com>
2014-01-22 23:39:56 -0500

    Commit 3: Removed AutoPublish & Added "Manual" Pub/Sub

--- proteinTracker.js
+++ proteinTracker.js
@@ -3,6 +3,10 @@ Users = new Meteor.Collection('users');
 History = new Meteor.Collection('history');
 
 if (Meteor.isClient) {
+
+Meteor.subscribe('allUsers');
+Meteor.subscribe('allHistory');
+
   Template.userDetails.helpers({
     user: function () {
       return Users.findOne();
@@ -11,14 +15,14 @@ if (Meteor.isClient) {
 
   Template.history.helpers({
     historyItem: function() {
-      
+
       return History.find({}, {sort: {date: -1}, limit: 5 });
     }
   });
 
   Template.userDetails.events({
     'click #addAmount' : function (e) {
-      e.preventDefault()
+      e.preventDefault();
 
       var amount = parseInt($('#amount').val());
 
@@ -27,13 +31,22 @@ if (Meteor.isClient) {
       History.insert({
         value: amount,
         date: new Date().toTimeString(),
-        uerId: this._id
+        userId: this._id
       });
     }
   });
 }
 
 if (Meteor.isServer) {
+
+  Meteor.publish('allUsers', function(){
+    return Users.find();
+  });
+
+  Meteor.publish('allHistory', function(){
+    return History.find({}, {sort: {date: -1}, limit: 5});
+  });
+
   Meteor.startup(function () {
     // code to run on server at startup
     if (Users.find().count() === 0) {
